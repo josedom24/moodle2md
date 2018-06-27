30ea422e58bfc4189e1831c0b5f71e967e2550df
josedom24 <josedom24@gmail.com>
2018-06-27 18:03:04 +0200

    rewrite

--- rewrite/rewrite.py
+++ rewrite/rewrite.py
@@ -6,6 +6,7 @@ import os,shutil
 import unicodedata
 import argparse
 import zipfile
+import requests
 
 
 
@@ -19,9 +20,12 @@ def elimina_tildes(cadena):
     return s
 
 def elimina_caracteres_nombre_fichero(nomfich):
-    car=("/","?","(",")"," ")
+    car=("/","?","(",")"," ",",",".")
     for c in car:
         nomfich=nomfich.replace(c,"_")
+    if nomfich[-1]=="_":
+        nomfich=nomfich[:-1]
+    nomfich=nomfich.replace("__","_")
     return nomfich
 
 def crear_fichero(fich,dir):
@@ -50,7 +54,7 @@ def copiar_imagenes(DIR_COPIA,DIR):
 
 
 
-def getSeccionesActividades():
+def getSeccionesActividades(lista):
 
     doc = etree.parse('moodle_backup.xml')
     secciones=doc.find("information/contents/sections")
@@ -66,13 +70,13 @@ def getSeccionesActividades():
             #    getLabel(actividad,DIR_COPIA,DIR,FICHERO)
                 pass
             elif tipo=="url":
-                getUrl(actividad)
+                getUrl(actividad,lista)
             elif tipo=="assign":
-                getAssign(actividad)
+                getAssign(actividad,lista)
             elif tipo=="resource":
-                getResource(actividad)
+                getResource(actividad,lista)
             elif tipo=="page":
-                getPage(actividad)  
+                getPage(actividad,lista)  
             
 #def getLabel(actividad,DIR_COPIA,DIR,FICHERO):
 #    doclabel=etree.parse(DIR_COPIA+"/%s/label.xml" % actividad.find("directory").text)
@@ -80,29 +84,29 @@ def getSeccionesActividades():
 #    escribir(DIR,FICHERO,"#### %s" % images(doclabel.find("label/intro").text))
 #    escribir(DIR,FICHERO)
 #
-def getUrl(actividad):
+def getUrl(actividad,lista):
     docactivity=etree.parse("%s/url.xml" % actividad.find("directory").text)
-    print "mod/url/view.php?id=%s -> %s"%(actividad.find("moduleid").text,docactivity.find("url/externalurl").text)
+    lista.append(["mod/url/view.php?id=%s"%actividad.find("moduleid").text,docactivity.find("url/externalurl").text])
 
-def getAssign(actividad):
+def getAssign(actividad,lista):
     nomfich=getNombreFichero(actividad.find("title").text)
-    print "mod/assign/view.php?id=%s -> %s"%(actividad.find("moduleid").text,"doc/"+nomfich)
+    lista.append(["mod/assign/view.php?id=%s"%actividad.find("moduleid").text,"doc/"+nomfich])
 
-def getResource(actividad):
+def getResource(actividad,lista):
 
     docresource=etree.parse("%s/resource.xml" % actividad.find("directory").text)
     fileid=docresource.getroot().get("contextid")
     docfiles=etree.parse("files.xml")
-    fichero=docfiles.xpath("//file[contextid=%s]"%fileid)   
+    fichero=docfiles.xpath("//file[contextid=%s]"%fileid)
     try:
-        print "mod/resource/view.php?id=%s -> %s"%(actividad.find("moduleid").text,"files/"+fichero[0].find("filename").text)
+        lista.append(["mod/resource/view.php?id=%s"%actividad.find("moduleid").text,"files/"+fichero[0].find("filename").text])
     except:
         pass
 
-def getPage(actividad):
+def getPage(actividad,lista):
     docpage=etree.parse("%s/page.xml" % actividad.find("directory").text)    
     nomfich=getNombreFichero(actividad.find("title").text)
-    print "mod/page/view.php?id=%s -> %s"%(actividad.find("moduleid").text,"doc/"+nomfich)
+    lista.append(["mod/page/view.php?id=%s"%actividad.find("moduleid").text,"doc/"+nomfich])
 #    try:
 #        escribir(DIR+"doc/",nomfich,"# %s" % actividad.find("title").text)
 #        escribir(DIR+"doc/",nomfich,images(docpage.find("page/content").text))
@@ -112,9 +116,21 @@ def getPage(actividad):
 
 
 def main():
-    
-    getSeccionesActividades()
+    lista=[]
+    getSeccionesActividades(lista)
+    for l in lista:
+        if "files/" in l[1] or "doc/" in l[1]:
+            base="http://plataforma2.josedomingo.org/pledin/cursos/"
+            base2="mv_portal_educativo_2006/"
+            r=requests.get(base+base2+l[1].replace(".md",".html"))
+            print r.url,r.status_code
 
+    for l in lista:
+        print 'RewriteCond %%{QUERY_STRING} id=%s'%l[0].split("=")[1]
+        if "url/" in l[0]:
+            print 'RewriteRule ^%s$ %s'%(l[0].split("?")[0],l[1]+" [R]")
+        else:
+            print 'RewriteRule ^%s$ %s'%(l[0].split("?")[0],base+base2+l[1].replace(".md",".html")+" [R]")
 
 
 if __name__ == '__main__':
